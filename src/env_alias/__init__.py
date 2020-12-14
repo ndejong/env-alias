8998ea09fc869394406be72b162d3b68f473fcda
Nicholas de Jong <me@nicholasdejong.com>
2020-12-14 18:52:40 +1000

    fixing broken merge

--- src/env_alias/__init__.py
+++ src/env_alias/__init__.py
@@ -3,11 +3,7 @@
 
 __title__ = "env-alias"
 __author__ = "Nicholas de Jong <contact@nicholasdejong.com>"
-<<<<<<< HEAD
 __version__ = '0.3.4'
-=======
-__version__ = '0.3.0'
->>>>>>> 18fafcd6b250d053d6f854f154623e33601fe2b9
 __license__ = "BSD2"
 
 __logger_default_level__ = 'info'
