61bdc9fed7503e71074b913e4c78156c654339d9
Nicholas de Jong <ndejong@threatpatrols.com>
2024-09-28 17:24:18 +1000

    release 0.5.3

--- src/env_alias/__init__.py
+++ src/env_alias/__init__.py
@@ -6,7 +6,7 @@ from os import getenv
 from sys import argv
 
 __title__ = "Env Alias"
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 
 LOGGER_LEVEL = "info"
 if "--debug" in argv or getenv("ENVALIAS_DEBUG", "").lower().startswith(("true", "yes", "enable", "on")):
